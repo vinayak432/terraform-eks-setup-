4f7822cf97eda76be9a028f96c271be6ae2e54ce
EC2 Default User <ec2-user@ip-172-31-13-13.ap-south-2.compute.internal>
2026-06-20 12:52:05 +0000

    instance type updated

--- eks.tf
+++ eks.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_cluster" "main" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_cluster.arn
-  version  = "1.29"
+  version  = "1.31"
 
   vpc_config {
     subnet_ids = [
