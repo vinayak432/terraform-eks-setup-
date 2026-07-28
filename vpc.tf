1e717f1939fcdd9078e57ebc2a702c6f4fdd478d
vinayak432 <53861374+vinayak432@users.noreply.github.com>
2026-07-28 08:00:02 +0530

    Fix formatting of vpc_id assignment in vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id 
 
   tags = {
     Name = "${var.cluster_name}-igw"
